677708e7dc1cbb2e993a231cb7c661364e28ad39
piyush kasturi <piyush5kasturi@gmail.com>
2024-08-10 18:24:21 +0530

    remove extra classes

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -22,12 +22,12 @@ export default function Home() {
         Memory Game
       </p>
       <div className="flex flex-col gap-2 w-96 mt-48 px-6">
-        <label className="mb-2 text-sm font-medium text-gray-900 dark:text-white w-48 flex items-center">
+        <label className="mb-2 text-sm font-medium text-gray-900 w-48 flex items-center">
           Enter Grid Size
         </label>
         <input
           type="number"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           onChange={(e) => {
             setError("");
             setNumber(e.target.value);
